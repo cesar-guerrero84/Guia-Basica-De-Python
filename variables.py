fef6173e8cc025d5c5c9f6f2ff09076d52dd42f3
cesar guerrero <guerrerofernandez007@gmail.com>
2020-09-15 14:50:43 -0500

    el primer commit

---
+++ variables.py
@@ -0,0 +1,11 @@
+#las variables es un nombre simbolico para guardar cualquier tipo de valor/dato y usarlo en cualquier momento recuerda que las mayusculas son importantes (case sensitive) y no es valido empezar una variable con un numero solo con caracteres tipo letra o guiones
+
+name = "cesar"
+Name = "cesar en mayuscula" #como name empieza con N mayuscula es otra variable difetente al primer name
+numero = 150
+diccionario ={
+    "libro":"fortaleza digital",
+    "año":"1998"
+}
+
+print(name,Name,numero,diccionario) #imprime en la consola todas las variables anteriores
