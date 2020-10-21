21c9681b6ffea8e68c614eb285a6edd0004f0c1e
cesar guerrero <guerrerofernandez007@gmail.com>
2020-10-20 23:43:21 -0500

    se agrego la clase diccionarios

---
+++ 9-diccionarios.py
@@ -0,0 +1,12 @@
+# los diccionarios son para guardar varios datos de un solo grupo por ejemplo los productos de un carrito de compras o una lista de una persona con sus datos personales
+
+producto = {
+    "nombre" : "libro",
+    "cantidad" : 4,
+    "precio" : 4.500
+}
+persona = {
+    "nombre" : "juan",
+    "edad" : 25,
+    "nacionalidad" : "colombiana"
+}
